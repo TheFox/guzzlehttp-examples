f0c8d216339d83142dca1129d0838df268467987
Christian Mayer <christian@fox21.at>
2017-02-07 15:43:14 +0100

    v5 JSON example added.

--- v5/post.php
+++ v5/post.php
@@ -9,9 +9,15 @@ $client = new GuzzleClient();
 
 print 'send request ... ';
 try{
+	// Raw Body
 	$response = $client->post('https://httpbin.org/post', array(
 		'body' => 'raw data',
 	));
+	
+	// JSON Body
+	// $response = $client->post('https://httpbin.org/post', array(
+	// 	'json' => array('hello' => 'world'),
+	// ));
 }
 catch(GuzzleRequestException $e){
 	if($e->hasResponse()){
@@ -23,4 +29,5 @@ print 'done'."\n";
 print 'response: '.get_class($response)."\n";
 print '  status: '.$response->getStatusCode()."\n";
 print '  header: '.$response->getHeader('content-type')."\n";
-print '  body:'."\n".$response->getBody()."\n";
+print '  body:'."\n".(string)$response->getBody()."\n";
+// print '  json body:'."\n".(string)$response->json()."\n";
